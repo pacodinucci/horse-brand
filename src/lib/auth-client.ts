aeb1eb433dc575d8d960c52912c5db82e1cd0609
pacodinucci <franciscoldinucci@gmail.com>
2025-08-14 15:26:17 -0300

    baseurl fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   // baseURL: "http://localhost:3000",
   // baseURL: "https://4c71c05ac111.ngrok-free.app",
-  baseURL: "https://8cd24c98782f.ngrok-free.app/",
-  // baseURL: "https://horse-brand.vercel.app",
+  // baseURL: "https://8cd24c98782f.ngrok-free.app/",
+  baseURL: "https://horse-brand.vercel.app",
 });
