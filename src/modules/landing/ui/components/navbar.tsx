ca2a69058e8787ba21eaba474a059dd17a32fd84
pacodinucci <franciscoldinucci@gmail.com>
2025-10-11 17:38:08 -0300

    links a skeletons

--- src/modules/landing/ui/components/navbar.tsx
+++ src/modules/landing/ui/components/navbar.tsx
@@ -1,12 +1,26 @@
 import Image from "next/image";
 import { NavbarCategories } from "./navbar-categories";
 import { NavbarButtons } from "./navbar-buttons";
+import Link from "next/link";
 
 export const Navbar = () => {
   return (
     <nav>
-      <div className="w-full text-center py-1 text-white text-sm uppercase tracking-wide bg-slate-950">
-        Esta página es provisoria hasta tener diseño
+      <div className="w-full flex justify-center gap-6 text-center py-1 text-white text-sm uppercase tracking-wide bg-slate-950">
+        <p>Links a skeletons &gt; </p>
+        <div>
+          <Link href={"/structure"} className="underline">
+            Landing
+          </Link>{" "}
+          &gt;{" "}
+          <Link href={"/structure/category"} className="underline">
+            Category
+          </Link>{" "}
+          &gt;{" "}
+          <Link href={"/structure/product"} className="underline">
+            Product
+          </Link>
+        </div>
       </div>
       <div className="bg-[var(--var-brown)] flex justify-between items-center px-6">
         <Image
